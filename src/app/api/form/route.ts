874a5bd7adb5f86d27402fbe32751f6c077bba06
Binshad K <binshadcs@gmail.com>
2024-10-06 22:16:38 +0530

    chore: prisma added

--- src/app/api/form/route.ts
+++ src/app/api/form/route.ts
@@ -3,3 +3,6 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET() {
   return NextResponse.json({ message: "server is up!" });
 }
+export async function POST() {
+    return NextResponse.json({ message: "server is up from post!" });
+}
